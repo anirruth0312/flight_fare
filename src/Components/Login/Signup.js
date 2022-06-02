0ab7df253daf3445cb11fd222d366d19a4bf9a09
Anirruth <anirruthbun@gmail.com>
2022-06-02 22:55:38 +0530

    Added signup link

---
+++ src/Components/Login/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <a href="https://www.youtube.com/">
+      <u>
+        <h3>Click here to signup</h3>
+      </u>
+    </a>
+  );
+}
+
+export default Signup;
